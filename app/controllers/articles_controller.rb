8bae7ace6f630a56ac4b64692f61073616eafcbf
enricotj <taiphoon007@gmail.com>
2020-05-10 12:26:11 -0400

    Add basic CRUD functions, views, etc.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,56 @@
 class ArticlesController < ApplicationController
+
+  # CREATE
   def new
+    @article = Article.new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+
   end
+
+  # READ
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  # UPDATE
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+  end
+
+  # DESTROY
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  # Validate that each article has a title that is at least 5 characters long
+  validates :title, presence: true, length: { minimum: 5 }
 end
