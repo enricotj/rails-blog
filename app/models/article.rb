0fc03dc0b6325b2f4eb0018287435014652876c6
enricotj <taiphoon007@gmail.com>
2020-05-10 17:33:27 -0400

    Add create and delete controller methods for comments. Adjust views appropriately.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
 
   # Associate an article with many comments
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   # Validate that each article has a title that is at least 5 characters long
   validates :title, presence: true, length: { minimum: 5 }
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
+
+  # Validate that each comment has a commenter name that is at least 3 characters long
+  validates :commenter, presence: true, length: { minimum: 3 }
 end
